d311d9350640437b44ac396b69a9afba4c1ba89a
David <sanderdj90@gmail.com>
2024-07-24 17:08:13 -0600

    feat: add route

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ def hello_world():
     return 'Hello, World!'
 
 
+@app.route('/semantic-release')
+def semantic_release():
+    return 'Semantic Release'
+
 if __name__ == '__main__':
     app.run(debug=True)
